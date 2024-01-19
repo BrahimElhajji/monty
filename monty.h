76a0f9bff987ac5a7bf77cf270ae6e2aab4dbbf0
BrahimElhajji <elhajji080898@gmail.com>
2024-01-18 20:52:41 -0800

    rotl

--- monty.h
+++ monty.h
@@ -62,6 +62,7 @@ void my_div(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 int exec(char *cont, stack_t **stack, unsigned int c, FILE *file);
 void pop(stack_t **stack, unsigned int line_number);
 char *strdup_custom(const char *str);

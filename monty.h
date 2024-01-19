27a0810917ac417d6f09703b31de24fb02c37b77
BrahimElhajji <elhajji080898@gmail.com>
2024-01-18 20:00:40 -0800

    nop

--- monty.h
+++ monty.h
@@ -55,6 +55,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 int exec(char *cont, stack_t **stack, unsigned int c, FILE *file);
 void pop(stack_t **stack, unsigned int line_number);
 char *strdup_custom(const char *str);

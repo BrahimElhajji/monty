b3cc3c8e1f111556c53ae4a8eeab4d71ce4f275b
BrahimElhajji <elhajji080898@gmail.com>
2024-01-21 18:47:48 -0800

    handling pop.c

---
+++ pop.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: pointer to pointer to the stack head
+ * @line_number: the number of lines
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(file);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+
+	if (*stack)
+		(*stack)->prev = NULL;
+
+	free(temp);
+}
